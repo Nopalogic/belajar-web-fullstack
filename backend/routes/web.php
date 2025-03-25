657fb3e8fec012b0b0f387c1fb9eabdf57a05044
Nopalogic <naufaladhi02@gmail.com>
2025-03-25 10:02:01 +0700

    Instalation and  introduction of views, routing, dan controllers

---
+++ backend/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\LoginController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/profile', function () {
+    return view('profile.index');
+});
+
+Route::get('/login', [LoginController::class, 'login_view'])->name('login_view');
+
+Route::get('/register', [LoginController::class, 'register_view'])->name('register_view');
