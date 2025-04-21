5b7687f255cd4d0a8eb0d785187d0d73ffa50a31
Nopalogic <naufaladhi02@gmail.com>
2025-04-21 08:03:39 +0700

    Authentication

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CourseController;
 use App\Http\Controllers\Api\LecturerController;
 use App\Http\Controllers\Api\MajorController;
@@ -17,19 +18,24 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::post('/auth/register', [AuthController::class,'register']);
+Route::post('/auth/login', [AuthController::class,'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/auth/logout', [AuthController::class,'logout']);
+
+    Route::get('/students', [StudentController::class, 'index']);
+    Route::post('/students', [StudentController::class, 'store']);
+    Route::get('/students/{id}', [StudentController::class, 'show']);
+    Route::put('/students/{id}', [StudentController::class, 'update']);
+    Route::delete('/students/{id}', [StudentController::class, 'delete']);
+    
+    Route::get('/majors', [MajorController::class, 'index']);
+    Route::post('/majors', [MajorController::class, 'store']);
+    
+    Route::get('/courses', [CourseController::class, 'index']);
+    Route::post('/courses', [CourseController::class, 'store']);
+    
+    Route::get('/lecturers', [LecturerController::class, 'index']);
+    Route::post('/lecturers', [LecturerController::class, 'store']);
 });
-
-Route::get('/students', [StudentController::class, 'index']);
-Route::post('/students', [StudentController::class, 'store']);
-
-Route::get('/majors', [MajorController::class, 'index']);
-Route::post('/majors', [MajorController::class, 'store']);
-
-Route::get('/courses', [CourseController::class, 'index']);
-Route::post('/courses', [CourseController::class, 'store']);
-
-Route::get('/lecturers', [LecturerController::class, 'index']);
-Route::post('/lecturers', [LecturerController::class, 'store']);
